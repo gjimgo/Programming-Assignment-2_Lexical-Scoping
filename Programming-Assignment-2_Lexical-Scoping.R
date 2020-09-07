52c062721ef8e071c49caf2a7c7f633bddf7d508
gjimgo <gjimenez@unab.edu.co>
2020-09-06 21:19:52 -0500

    Initial commit of Programming-Assignment-2_Lexical-Scoping.R

---
+++ Programming-Assignment-2_Lexical-Scoping.R
@@ -0,0 +1,28 @@
+#MakeCacheMatrix function creates a special “matrix” object that can cache its inverse.
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+#Cachemean function computes the inverse of the special “matrix” returned by makeCacheMatrix above
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
